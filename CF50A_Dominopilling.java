7b2eb8955d9175569d6bd2da03dff35e3d190485
Udhaya Nithi <udhxyanithi.1@gmail.com>
2026-08-08 20:23:58 +0530

    Add Codeforces 50A - Domino pilling solution

---
+++ CF50A_Dominopilling.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CF50A_Dominopilling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int result = (m * n) / 2;
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
