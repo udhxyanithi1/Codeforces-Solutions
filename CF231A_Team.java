47ed0d6f767434c0ec3de54594f4392afd11d753
Udhaya Nithi <udhxyanithi.1@gmail.com>
2026-08-08 20:15:15 +0530

    Add Codeforces 231A - Team solution

---
+++ CF231A_Team.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CF231A_Team {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int petya = sc.nextInt();
+            int vasya = sc.nextInt();
+            int tonya = sc.nextInt();
+
+            if (petya + vasya + tonya >= 2) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
