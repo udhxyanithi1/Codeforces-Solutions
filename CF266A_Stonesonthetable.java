3841c83edc6de468349e463b200936e01a1d6aeb
Udhaya Nithi <udhxyanithi.1@gmail.com>
2026-08-19 17:33:26 +0530

    Add Codeforces 266A Stones on the Table solution

---
+++ CF266A_Stonesonthetable.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class CF266A_Stonesonthetable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String s = sc.next();
+
+        int count = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
