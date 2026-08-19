4cd42842cc05f5060d6df76f1d89d9e3e694b177
Udhaya Nithi <udhxyanithi.1@gmail.com>
2026-08-19 20:01:18 +0530

    Add Codeforces 141A Amusing Joke solution

---
+++ CF141A_Amusingjoke.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class CF141A_Amusingjoke {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String guest = sc.nextLine();
+        String host = sc.nextLine();
+        String pile = sc.nextLine();
+
+        String combined = guest + host;
+
+        char[] a = combined.toCharArray();
+        char[] b = pile.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if (Arrays.equals(a, b)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
