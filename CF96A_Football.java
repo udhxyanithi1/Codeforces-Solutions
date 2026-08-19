69b4224be8fff71fac7f0becf0a99e2013b6216b
Udhaya Nithi <udhxyanithi.1@gmail.com>
2026-08-19 17:28:51 +0530

    Add Codeforces 96A Football solution

---
+++ CF96A_Football.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class CF96A_Football {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        int count = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count >= 7) {
+                System.out.println("YES");
+                return;
+            }
+        }
+
+        System.out.println("NO");
+    }
+}
