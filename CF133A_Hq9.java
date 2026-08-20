cd3c01f7a9e9d03d0aad6330d324e32f9426c7a9
Udhaya Nithi <udhxyanithi.1@gmail.com>
2026-08-20 19:38:04 +0530

    Add Codeforces 133A Hq9++ solution

---
+++ CF133A_Hq9.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class CF133A_Hq9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String p = sc.nextLine();
+
+        if (p.contains("H") || p.contains("Q") || p.contains("9")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        sc.close();
+    }
+}
