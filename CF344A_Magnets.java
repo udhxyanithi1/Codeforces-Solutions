d2af7333b2d6010e9225e82de39ba6a3234d84fa
Udhaya Nithi <udhxyanithi.1@gmail.com>
2026-08-23 20:36:41 +0530

    Add Codeforces 344A Magnets solution

---
+++ CF344A_Magnets.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class CF344A_Magnets {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    String previous = sc.next();
+
+    int groups = 1;
+
+    for (int i = 1; i < n; i++) {
+      String current = sc.next();
+
+      if (!current.equals(previous)) {
+        groups++;
+      }
+
+      previous = current;
+    }
+
+    System.out.println(groups);
+
+    sc.close();
+  }
+}
