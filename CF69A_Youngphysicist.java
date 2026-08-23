ce9d8feaca031c92f9ab77c0d471db9e093b6761
Udhaya Nithi <udhxyanithi.1@gmail.com>
2026-08-23 20:33:41 +0530

    Add Codeforces 69A Young Physicist solution

---
+++ CF69A_Youngphysicist.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class CF69A_Youngphysicist {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    int x = 0;
+    int y = 0;
+    int z = 0;
+
+    for (int i = 0; i < n; i++) {
+      x += sc.nextInt();
+      y += sc.nextInt();
+      z += sc.nextInt();
+    }
+
+    if (x == 0 && y == 0 && z == 0) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+
+    sc.close();
+  }
+}
