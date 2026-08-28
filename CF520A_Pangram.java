502057ee1da17d96f4fce1ca86387ac43be8ef5e
Udhaya Nithi <udhxyanithi.1@gmail.com>
2026-08-28 18:23:23 +0530

    Add Codeforces 520A Pangram solution

---
+++ CF520A_Pangram.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class CF520A_Pangram {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    String s = sc.next();
+
+    boolean[] letters = new boolean[26];
+
+    for (int i = 0; i < s.length(); i++) {
+      char c = Character.toLowerCase(s.charAt(i));
+      letters[c - 'a'] = true;
+    }
+
+    for (int i = 0; i < 26; i++) {
+      if (!letters[i]) {
+        System.out.println("NO");
+        sc.close();
+        return;
+      }
+    }
+
+    System.out.println("YES");
+
+    sc.close();
+  }
+}
